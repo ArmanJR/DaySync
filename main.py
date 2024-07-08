5615f443c6b541759aaa61e03d69b6823e2d019b
Arman Jafarnezhad <arman.jafarnezhad@adevinta.com>
2024-07-08 17:39:28 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,102 @@
+import os
+from waveshare import epd2in13_V3
+from PIL import Image, ImageDraw, ImageFont
+
+import os.path
+import time
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+import datetime
+import pytz
+
+font24 = ImageFont.truetype('waveshare/Font.ttc', 24)
+
+epd = epd2in13_V3.EPD()
+epd.init()
+epd.Clear(0xFF)
+
+# Set the timezone to your local timezone
+tz = pytz.timezone('America/Toronto')
+# Fixed port is needed when whitelisting redirect url on Google cloud
+# Make sure this port is already open
+PORT = 8999
+# The calendar ID of the calendar you want to monitor
+# If you want to monitor your own Google calendar, set this to 'primary'
+calendarID = 'primary'
+updateInterval = 600  # 10 minutes
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+def fetch_calendar_events():
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_secret.json', SCOPES)
+            creds = flow.run_local_server(port=PORT)
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    now = datetime.datetime.now(tz)
+    ten_minutes_ago = (now - datetime.timedelta(minutes=10)).isoformat()
+    today_evening = now.replace(hour=20, minute=0, second=0, microsecond=0).isoformat()
+
+    events_result = service.events().list(calendarId=calendarID, timeMin=ten_minutes_ago,
+                                          timeMax=today_evening,
+                                          maxResults=10, singleEvents=True,
+                                          orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    return events
+
+
+def format_events(events):
+    formatted_events = []
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        end = event['end'].get('dateTime', event['end'].get('date'))
+        summary = event.get('summary', 'No Title')
+
+        start_time = datetime.datetime.fromisoformat(start).strftime('%H:%M')
+        end_time = datetime.datetime.fromisoformat(end).strftime('%H:%M')
+
+        formatted_events.append(f"{start_time} => {end_time} [{summary}]")
+        # Sometimes you don't have access to the shared calendars events names:
+        # formatted_events.append(f"{start_time} => {end_time}")
+    return formatted_events
+
+
+def monitor_calendar_events():
+    last_events = None
+
+    while True:
+        current_events = fetch_calendar_events()
+        formatted_events = format_events(current_events)
+
+        if formatted_events != last_events:
+            draw_texts(formatted_events)
+            last_events = formatted_events
+
+        time.sleep(updateInterval)  # Wait for 10 minutes
+
+
+def draw_texts(texts):
+    image = Image.new('1', (epd.height, epd.width), 255)
+    draw = ImageDraw.Draw(image)
+    for i, text in enumerate(texts):
+        draw.text((1, 1 + i * 30), text, font=font24, fill=0)  # [1,1] is the top left corner of the text
+    epd.display(epd.getbuffer(image))
+
+
+if __name__ == '__main__':
+    monitor_calendar_events()
---
+++ readme.md
@@ -0,0 +1,72 @@
+# DaySync
+DaySync is a small e-ink display on your desk. It has only one purpose: show your upcoming events and meets from your Google Calendar.
+
+![device](docs/image.jpg)
+
+## Device
+Any standard Wi-Fi enabled [Raspberry Pi](https://a.co/d/06YPsAjF) 40PIN GPIO (tested on 3B+) and the [Waveshare 2.13 inch e-Paper Display Hat](https://a.co/d/0hyt380P).
+
+## Getting Started
+0. Clone the repo 
+```
+git clone https://github.com/ArmanJR/DaySync
+cd DaySync
+```
+1. Go to your Google cloud console and create a project
+2. Enable [Google Calendar API](https://console.cloud.google.com/marketplace/product/google/calendar-json.googleapis.com)
+3. Create [OAuth Client](https://console.cloud.google.com/apis/credentials/oauthclient)
+    - Application type: Web Application
+    - Add `http://localhost:8999` to Authorized JavaScript origins and Authorized redirect URIs
+    - Download the token, save it as `client_secret.json` alongside this repo's `main.py`
+4. Add your account as a test user on [OAuth consent screen](https://console.cloud.google.com/apis/credentials/consent)
+5. Create a virtual env 
+```
+python3 -m venv venv
+```
+6. Activate the env
+```
+source venv/bin/activate
+```
+7. Install the requirements
+```
+pip install -r requirements.txt
+```
+
+8. Modify `main.py` to your settings
+   - Your timezone `tz = pytz.timezone('America/Toronto')`
+   - The `calendarID` (more about calendars below)
+   - `updateInterval` in seconds
+
+9. Run the script to generate token
+```
+python main.py
+```
+A browser tab will open. Grant access to the app and close the browser after you did. A `token.json` file should exist now in the project directory.
+9. Run the script via nohup and monitor the output
+```
+nohup python main.py > output.log 2>&1 &
+```
+If you want to terminate the script, just find the PID and kill
+```
+ps aux | grep main.py
+```
+```
+user      12345  0.1  1.0  123456  12345 ?        S    10:00   0:00 python main.py
+user      12346  0.0  0.0  123456   1234 pts/0    S+   10:00   0:00 grep --color=auto main.py
+```
+```
+kill 12345
+```
+   
+## Calendars
+By default, your main calendar is called `primary`. You may want to import calendars from other accounts or sources.
+To do so, [share your calendar](https://support.google.com/calendar/answer/37082?hl=en) and replace `calendarID` with the one you added.
+
+## Improvements
+- [ ] Replace `nohup` with a proper script execution
+
+## Contribution
+Pull requests are welcome
+
+## License
+MIT
